1ee62348e3c921531898a71fe97e7767a70b63b7
Jason <jajones414@gmail.com>
2022-12-02 17:15:52 -0600

    I have added more files and boilerplate to the project.

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const path = require('path')
 const express = require('express')
 const exphbs = require('express-handlebars')
-const sequelize = require('./config/connection')
 const routes = require('./controllers')
 const helpers = require ('./utils/helpers')
+const session = require('express-session')
+
+const sequelize = require('./config/connection')
+
+// Reliably stores session
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express()
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers })
+
+// TODO: configure the session
+// const sess = {
+//     secret: 'Super secret secret',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//       db: sequelize
+//     })
+//   };
+  
+  // TODO: Get access to cookie ID
+  app.use(session(sess));
 
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
