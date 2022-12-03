9bf0d24e8ebf6e24374b760c55be7fc256e2df97
Jason <jajones414@gmail.com>
2022-12-03 10:17:28 -0600

    I have added index, user, post, and comment models to the project.

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const Post = require('./Post')
+const User = require('./User')
+const Comment = require('./Comment')
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Post.belongsTo(User, {
+    foreignKey: "user_id"
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey:'post_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+module.exports = {User, Post, Comment}
