16f7dbcf6f2d12c090cc66bdd5783aa8080a72fc
Jason <jajones414@gmail.com>
2022-12-09 22:07:08 -0600

    I have corrected navbar paths and removed the index.h.handlebars file.

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const postRoutes = require('./postRoutes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('./posts', postRoutes)
 
 module.exports = router;
--- public/js/logout.js
+++ public/js/logout.js
@@ -7,7 +7,7 @@ const logout = async () => {
 
   if (response.ok) {
     // If successfully logged out, redirect to the login page
-    document.location.replace('/index.handlebars');
+    document.location.replace('/login');
   } else {
     alert(response.statusText);
   }
